81acda8980acbe8dbdd9f3a73e79ad0ef8f2e625
patricia <patysuarez28@hotmail.com>
2021-06-18 17:23:38 -0500

    cambio pedido

--- src/projectgit/carac1.java
+++ src/projectgit/carac1.java
@@ -3,7 +3,7 @@ package projectgit;
 public class carac1 {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo desde remoto");
+		System.out.println("hola mundo desde remoto lo que me han pedido");
 
 	}
 
